b9fb8fd3547ed1256a8a47c80e576674a1431a84
Pur <purwantorpl@gmail.com>
2024-02-26 17:11:54 +0700

    Stateful & stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:new_flutter/widgets/soal24.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int nilai = 0;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: Soal24());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Increment Apps'),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    nilai--;
+                  });
+                  print('$nilai');
+                },
+                child: const Icon(Icons.remove)),
+            Text(
+              '$nilai',
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    nilai++;
+                  });
+                  print('$nilai');
+                },
+                child: const Icon(Icons.add)),
+          ],
+        )),
+      ),
+    );
   }
 }
